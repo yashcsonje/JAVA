05a340d93ae4bf90464a93568685e6cf2c8b84c5
yashcsonje <162028752+yashcsonje@users.noreply.github.com>
2024-09-29 15:09:42 +0530

    Add files via upload

---
+++ Strings/Concatenate.java
@@ -0,0 +1,8 @@
+public class Concatenate {
+    public static void main(String args[]){
+        String firstName = "Yash";
+        String lastName = "Sonje";
+        String fullName = firstName +" "+ lastName ;
+        System.out.println(fullName);
+    }
+}
